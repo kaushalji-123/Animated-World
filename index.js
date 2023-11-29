30035883c0b16463a3d87ca7f5c66bd054d6db0c
kaushalji-123 <divyanshu838283fb@gmail.com>
2023-11-30 01:02:46 +0530

    added navbar

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.calculate').addEventListener('click', (e) => {
+        console.log('Button clicked!');
+    });
+});
